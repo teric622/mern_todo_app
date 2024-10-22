e1fd8cadbb70ad8166baf4df00d3cd716cd3e182
teric622 <tlasecaeric@gmail.com>
2024-10-22 17:52:48 -0400

    updatecors

--- server.js
+++ server.js
@@ -10,11 +10,14 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// CORS options to allow requests from your frontend
+
 const corsOptions = {
-  origin: 'https://mern-todo-app-1-pwmp.onrender.com/', // Update to your frontend URL
-  credentials: true, // Optional: if you need to allow cookies or authentication headers
-};
+    origin: 'https://mern-todo-app-1-pwmp.onrender.com', // Make sure there are no trailing slashes
+    credentials: true, // Optional: if you need to allow cookies or authentication headers
+  };
+  
+  app.use(cors(corsOptions));
+  
 
 // Middleware
 app.use(cors(corsOptions));
